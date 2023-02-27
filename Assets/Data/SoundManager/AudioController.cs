20fe7e50d21a8070b1a05f2e64a93f9ba869577f
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-02-27 20:04:06 +0900

    [update] サウンドマネージャーを更新

--- Assets/Data/SoundManager/AudioController.cs
+++ Assets/Data/SoundManager/AudioController.cs
@@ -12,6 +12,11 @@ public class AudioController : MonoBehaviour
     [SerializeField]
     private Slider _seVolumeSlider = default;
 
+    private void Awake()
+    {
+        SliderInit(_bgmVolumeSlider, AudioManager.BGMVolume.Value);
+        SliderInit(_seVolumeSlider, AudioManager.SEVolume.Value);
+    }
     private void OnEnable()
     {
         _bgmVolumeSlider?.onValueChanged.AddListener(AudioManager.ChangeBGMVolume);
@@ -21,5 +26,17 @@ public class AudioController : MonoBehaviour
     {
         _bgmVolumeSlider?.onValueChanged.RemoveListener(AudioManager.ChangeBGMVolume);
         _seVolumeSlider?.onValueChanged.RemoveListener(AudioManager.ChangeSEVolume);
+    }
+    /// <summary>
+    /// スライダーの初期化処理
+    /// </summary>
+    /// <param name="slider"> 初期化対象のスライダー </param>
+    /// <param name="initializeValue"> スライダーの初期値 </param>
+    private void SliderInit(Slider slider, float initializeValue)
+    {
+        slider.minValue = 0f;
+        slider.maxValue = 1f;
+        slider.wholeNumbers = false;
+        slider.value = initializeValue;
     }
 }
